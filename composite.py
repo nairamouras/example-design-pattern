5ba2fe118c2f7f31aa7937d4c9b762e9621b69e9
Naíra de Moura e Souza <74928249+nairamouras@users.noreply.github.com>
2022-09-05 13:43:47 -0400

    Update composite.py

--- composite.py
+++ composite.py
@@ -4,9 +4,11 @@ from typing import List
 
 class Component(ABC):
 
+    @property
     def parent(self) -> Component:
         return self._parent
 
+    @parent.setter
     def parent(self, parent: Component):
         self._parent = parent
 
